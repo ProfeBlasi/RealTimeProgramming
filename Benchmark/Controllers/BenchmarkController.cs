a4b0be35c34dfd2c135b0ba3429b7fb7552aa604
Ezequiel Blasi <profeblasi@outlook.com>
2022-11-05 14:06:56 -0300

    Se agrega fecha

--- Benchmark/Controllers/BenchmarkController.cs
+++ Benchmark/Controllers/BenchmarkController.cs
@@ -9,7 +9,8 @@ namespace Benchmark.Controllers
         [HttpGet("/")]
         public string Get()
         {
-            return "Este es nuestro Benchmark para programacion en tiempo real segundo cuatrimestre 2022, Alumnos: Djirikian Damian y Blasi Ezequiel";
+            return "Este es nuestro Benchmark para programacion en tiempo real segundo cuatrimestre 2022, Alumnos: Djirikian Damian y Blasi Ezequiel"
+                + " \n la presentacion se va a realizar el dia " + DateTime.Now.ToString();
         }
     }
 }
